93f6b5b088f468a2d7b800552145d476b8e7f2ae
Meder <medi_medi@mail.ru>
2022-08-21 13:52:48 +0300

    feat: реализовать вставку фигур на ячеек

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,9 +1,11 @@
 import { Cell } from "../Cell";
 import { Colors } from "../Colors";
+import logo from '../../assets/black-bishop.png'
 
-export class Figure {
-  color: Colors;
-  logo: typeof logo | null;
+export class Figure { 
+  color: Colors; // в поле color запишем enum Colors - это примитивный перечисляемый тип (цвета)
+  logo: typeof logo | null; // у каждой фигуры будет какой-то логотип. Чтобы правильно указать тип, сделаем 
+  // typeof от того объекта который мы импортировали.
   cell: Cell;
   name: FigureNames;
   id: number;
